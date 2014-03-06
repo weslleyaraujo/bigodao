c3be7da5adc3e0ae2c8542431ee9fbb358674a7c
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-03-06 20:41:03 -0300

    lots and lots of changes

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
+require 'compass'
 
 class App < Sinatra::Base
+	configure do
+		set :sass, {:style => :compact, :debug_info => false}
+		Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+	end
+
+	get '/stylesheets/:name.css' do
+		content_type 'text/css', :charset => 'utf-8'
+		sass :"stylesheets/#{params[:name]}", Compass.sass_engine_options
+	end
+
 	get '/' do
 		'Hello Bigodão'
 	end
