274275ce0c13725fea701756a78f6ec0a659e591
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-03-06 23:07:48 -0300

    Just stop reading these for a while, ok..

--- app.rb
+++ app.rb
@@ -2,20 +2,6 @@ require 'sinatra'
 require 'compass'
 
 class App < Sinatra::Base
-	configure do
-		Compass.configuration do |config|
-			config.project_path = File.dirname(__FILE__)
-			config.sass_dir = 'public/stylesheets'
-		end
-
-		set :sass, Compass.sass_engine_options
-	end
-
-	get '/stylesheets/:name.css' do
-		content_type 'text/css', :charset => 'utf-8'
-		sass :"stylesheets/#{params[:name]}", Compass.sass_engine_options
-	end
-
 	get '/' do
 		'Hello Bigodão'
 	end
