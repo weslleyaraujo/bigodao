abf921f07d9a947699dbde078592efc91e8e044f
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-03-09 00:14:09 -0300

    Add html5 boilerplate

--- app.rb
+++ app.rb
@@ -1,10 +1,35 @@
 require 'sinatra'
-require 'compass'
+require 'sinatra/assetpack'
 
 class App < Sinatra::Base
-	get '/' do
-		'Hello Bigodão'
-	end
+  # asset pack config
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/javascripts',	from: 'public/javascripts'
+    serve '/stylesheets',	from: 'public/stylesheets'
+
+    js :app, [
+      '/javascripts/main.js',
+    ]
+
+    css :application, [
+      '/stylesheets/base/*.css',
+      '/stylesheets/layout/*.css',
+      '/stylesheets/module/*.css',
+      '/stylesheets/no-modular/*.css',
+      '/stylesheets/main*.css',
+    ]
+
+    js_compression  :jsmin
+    css_compression :simple
+  }
+
+  get '/' do
+    erb :index
+  end
 end
 
 # rerun only
