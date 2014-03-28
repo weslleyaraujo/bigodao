604fe215b083c46da2326e008f0d0db464130bc8
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-03-27 21:44:39 -0300

    model defautls

--- public/javascripts/models/movie.js
+++ public/javascripts/models/movie.js
@@ -5,4 +5,29 @@
  * */
 
 Application.Models = Application.Models || {};
-Application.Models.Movie = Backbone.Model.extend();
+Application.Models.Movie = Backbone.Model.extend({
+	MovieID: '',
+	State: '',
+	MovieUrl: '',
+	MovieTitle: '',
+	MovieTitleClean: '',
+	MovieYear: '',
+	DateUploaded: '',
+	DateUploadedEpoch: '',
+	Quality: '',
+	CoverImage: '',
+	ImdbCode: '',
+	ImdbLink: '',
+	Size: '',
+	SizeByte: '',
+	MovieRating: '',
+	Genre: '',
+	Uploader: '',
+	UploaderUID: '',
+	Downloaded: '',
+	TorrentSeeds: '',
+	TorrentPeers: '',
+	TorrentUrl: '',
+	TorrentHash: '',
+	TorrentMagnetUrl: ''
+});
